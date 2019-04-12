a7e324cab34e54ad37017ae73431fb6c1fba12ee
Alexandar Castaneda <alexthedar@gmail.com>
2019-04-12 15:15:09 -0700

    basic functionality.  no styling.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import GphApiClient from "giphy-js-sdk-core";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const client = GphApiClient(API_KEY);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      searchText: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    this.searchGifs("cats");
+  }
+
+  searchGifs(text) {
+    client
+      .search("gifs", { q: text, lang: "en" })
+      .then(response => {
+        this.setState({
+          gifs: response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value
+    });
+  }
+
+  handleSubmit() {
+    const { searchText } = this.state;
+    this.searchGifs(searchText);
+  }
+
   render() {
+    const { gifs } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" onChange={e => this.handleChange(e)} />
+        <button type="submit" onClick={() => this.handleSubmit()}>
+          Submit
+        </button>
+        {gifs.map(gif => {
+          return (
+            <div key={gif.id}>
+              <img src={gif.images.fixed_width_downsampled.url} alt="" />
+            </div>
+          );
+        })}
       </div>
     );
   }
